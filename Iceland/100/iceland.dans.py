8f5468d2e8c4dff9c09708d992fea6aff07cc88b
Sverrir Hákon <24295046+Svessinn@users.noreply.github.com>
2019-01-07 22:38:52 +0000

    Add files via upload

--- Iceland/100/iceland.dans.py
+++ Iceland/100/iceland.dans.py
@@ -1,13 +1,16 @@
 N, K = map(int, input().split())
 order = list(map(int, input().split()))
 
+def dunno(a, b):
+    return [a[b[i]-1] for i in range(len(a))]
+
 def ath(o, k):
     if k == 0:
         return list(range(1, len(o)+1))
     elif k % 2 == 1:
-        return [o[ath(o, k-1)[i]-1] for i in range(len(o))]
+        return dunno(o, ath(o, k-1))
     else:
         res = ath(o, k//2)
-        return [res[res[i]-1] for i in range(len(o))]
+        return dunno(res, res)
 
 print(' '.join(map(str, ath(order, K))))
