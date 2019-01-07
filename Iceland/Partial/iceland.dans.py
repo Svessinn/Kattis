66132d347b5bb69f19672ecc387dd733032b0a5a
Sverrir Hákon <24295046+Svessinn@users.noreply.github.com>
2019-01-07 22:21:19 +0000

    Delete iceland.dans.py

--- Iceland/Partial/iceland.dans.py
+++
@@ -1,7 +0,0 @@
-N, K = map(int, input().split())
-order = list(map(int, input().split()))
-lst = [i for i in range(1, N+1)]
-
-for j in range (0, K):
-    lst = [lst[i-1] for i in order]
-print(*lst, sep = ' ')
