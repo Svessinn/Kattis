87c73bad17ca60389be64fe4719cf49ca60fe04d
Sverrir Hákon <24295046+Svessinn@users.noreply.github.com>
2019-01-17 22:05:55 +0000

    Add files via upload

--- Open/Python 3/relocation.py
+++ Open/Python 3/relocation.py
@@ -1,14 +1,8 @@
-dct = {}
 N, Q = map(int, input().split())
 companies = list(map(int, input().split()))
-for i in range(N):
-    dct[i+1] = companies[i]
 for i in range(Q):
     z, C, X = map(int, input().split())
     if z == 1:
-        dct[C] = X
-        Vals = list(dct.values())
-    #print(dct)
+        companies[C-1] = X
     if z == 2:
-        #print(Vals)
-        print(abs(Vals[C-1] - Vals[X-1]))
+        print(abs(companies[C-1] - companies[X-1]))
