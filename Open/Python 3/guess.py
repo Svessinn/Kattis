c5f8cf263803bdf97f8c2a332a3dd78a77b71bc2
Sverrir Hákon <24295046+Svessinn@users.noreply.github.com>
2019-01-18 10:57:34 +0000

    Update guess.py

--- Open/Python 3/guess.py
+++ Open/Python 3/guess.py
@@ -1,11 +1,9 @@
-import sys
 mn = 0
 mx = 1001
 ans = ''
 for i in range(10):
     guess = (mx + mn)//2
     print(guess)
-    sys.stdout.flush()
     ans = str(input())
     if ans == 'correct':
         break
